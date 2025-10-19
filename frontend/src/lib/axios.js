d0b6c150437520a3ecb604e4b46077c0a6c1ad3a
Richa Sharangdhar <sharangdharsricha8@gmail.com>
2025-10-19 17:17:58 +0530

    changing the variable name

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = import.meta.env.VITE_API_KEY;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const axiosInstance = axios.create({
   baseURL: `${API_BASE_URL}/api`,
   withCredentials: true,
